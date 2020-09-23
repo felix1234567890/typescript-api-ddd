cd1a85d1c2326077af8a1f4e810879b6a6801b93
felix1234567890 <blueskyandsea01@gmail.com>
2020-09-23 20:34:16 +0200

    Added e2e test for deleting books

---
+++ __tests__/integration/books/DeleteBook.spec.ts
@@ -0,0 +1,45 @@
+import { Connection, createConnection, getRepository, Repository } from 'typeorm';
+import request from 'supertest';
+import app from '../../../src/shared/infra/http/app';
+import { Book } from '../../../src/modules/books/infra/typeorm/entity';
+import getToken from '../../helpers/getToken';
+
+let connection: Connection;
+let bookRepository: Repository<Book>;
+let token: string;
+let userId: number;
+
+describe('Delete book', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    bookRepository = getRepository(Book);
+  });
+  beforeEach(async () => {
+    const response = await request(app).post('/users').send({
+      name: 'Marko Lukin',
+      email: 'franelukin10@gmail.com',
+      password: 'tojeto123',
+    });
+    token = await getToken('franelukin10@gmail.com', 'tojeto123');
+    userId = response.body.id;
+  });
+  afterEach(async () => {
+    await connection.query('DELETE FROM books');
+    await connection.query('DELETE FROM users');
+  });
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should delete book by id', async () => {
+    const { id: bookId } = await bookRepository.save(
+      bookRepository.create({
+        title: 'Harry Potter',
+        description: 'This is description of a book I have read',
+        authorId: userId,
+      }),
+    );
+    const response = await request(app).delete(`/books/${bookId}`).set('Authorization', `Bearer ${token}`);
+    expect(response.status).toBe(204);
+  });
+});
